42e28498f63bc8fea7246ba866440261b87d3131
Lucas Salibian <lucas.salibian@gmail.com>
2017-11-02 12:38:14 -0700

    Attempting to fix install script

--- files/install_mediawiki.sh
+++ files/install_mediawiki.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/bash
 
+# A script to install MediaWiki. This script takes the same parameters
+# as install.php and are passed into it directly.
+
 VERSION="$1"
+
+# Takes a number like "1.29.5" and turns it into "1.29"
 VERSION_SHORT=$(echo "$VERSION" | sed -E 's/([0-9]+\.[0-9]+)+\.[0-9]+/\1/g')
+
+# Skip the first argument
+shift 1
+
+if [[ ! -e /var/lib/mediawiki ]]; then
+    wget -O '/tmp/mediawiki.tar.gz' "https://releases.wikimedia.org/mediawiki/$VERSION_SHORT/mediawiki-$VERSION.tar.gz"
+    tar xvf /tmp/mediawiki.tar.gz
+    mkdir /var/lib/mediawiki
+    mv mediawiki-*/* /var/lib/mediawiki
+    rm /tmp/mediawiki.tar.gz
+
+    php /var/lib/mediawiki/maintenance/install.php $@
+fi
 
 if [[ -e "/var/lib/mediawiki/version" ]]; then
     CURRENT=$(cat /var/lib/mediawiki/version)
@@ -9,19 +27,7 @@ if [[ -e "/var/lib/mediawiki/version" ]]; then
         exit 0
     fi
 
-    UPGRADING="true"
-fi
-
-if [[ -e /var/lib/mediawiki ]]; then
-    wget -O '/tmp/mediawiki.tar.gz' "https://releases.wikimedia.org/mediawiki/$VERSION_SHORT/mediawiki-$VERSION.tar.gz"
-    tar xvf /tmp/mediawiki.tar.gz
-    mkdir /var/lib/mediawiki
-    mv mediawiki-*/* /var/lib/mediawiki
-    rm /tmp/mediawiki.tar.gz
-fi
-
-if [[ -z $UPGRADING ]]; then
-    # TODO
-    echo "Not implemented"
-    exit 1
+    # We're preforming an upgrade
+    mkdir -p /var/lib/backup
+    tar -cvf - /var/lib/mediawiki/ | xz -9 > /var/lib/backup/backup-$(date +%s).tar.xz
 fi
--- manifests/install.pp
+++ manifests/install.pp
@@ -40,15 +40,41 @@ class mediawiki::install (
     require  => Package['php-xml'];
   }
 
-  file { '/usr/local/bin/install_mediawiki.sh':
+  file { '/usr/bin/install_mediawiki.sh':
     source => 'puppet:///modules/mediawiki/install_mediawiki.sh',
     mode   => '0755',
     owner  => root,
     group  => root,
-  } ->
+  }
+
+  if $secured {
+    $server_url = "https://$server"
+  } else {
+    $server_url = "http://$server"
+  }
+
   exec { 'install mediawiki':
-    require  => Class['apache'],
-    command  => "/bin/bash /usr/local/bin/install_mediawiki.sh $version";
+    require => [
+      Class['apache'],
+      File['/usr/bin/install_mediawiki.sh'],
+    ],
+    command => "/bin/bash /usr/local/bin/install_mediawiki.sh \
+      $version \
+      admin \
+      --pass admin \
+      --server $server_url \
+      --scriptpath $script_path \
+      --dbtype postgres \
+      --dbserver $db_server \
+      --installdbuser $db_username \
+      --installdbpass $db_password \
+      --dbname $db_name \
+      --dbuser $db_username \
+      --dbpass $db_password \
+      --confpath /var/lib/mediawiki \
+      --lang $language \
+      ",
+      creates => '/var/lib/mediawiki/LocalSettings.php';
   } ~>
   file { '/var/www/html/mediawiki':
     target    => '/var/lib/mediawiki',
