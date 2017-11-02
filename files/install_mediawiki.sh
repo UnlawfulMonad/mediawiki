773d97f2c8960b2733cdd5a558896cfd03726052
Lucas Salibian <lucas.salibian@gmail.com>
2017-11-02 13:00:46 -0700

    Parameter tricks

--- files/install_mediawiki.sh
+++ files/install_mediawiki.sh
@@ -8,17 +8,16 @@ VERSION="$1"
 # Takes a number like "1.29.5" and turns it into "1.29"
 VERSION_SHORT=$(echo "$VERSION" | sed -E 's/([0-9]+\.[0-9]+)+\.[0-9]+/\1/g')
 
-# Skip the first argument
-shift 1
-
 if [[ ! -d /var/lib/mediawiki ]]; then
     wget -O '/tmp/mediawiki.tar.gz' "https://releases.wikimedia.org/mediawiki/$VERSION_SHORT/mediawiki-$VERSION.tar.gz"
     tar xvf /tmp/mediawiki.tar.gz
     mkdir /var/lib/mediawiki
     mv mediawiki-*/* /var/lib/mediawiki
+
+    echo "Mediawiki in place"
     rm /tmp/mediawiki.tar.gz
 
-    php /var/lib/mediawiki/maintenance/install.php $@
+    php /var/lib/mediawiki/maintenance/install.php "${@:2}"
 
     echo "$VERSION" > /var/lib/mediawiki/version
 fi
